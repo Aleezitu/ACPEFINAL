8e395ad1f8ed1102422c07471e938b0f81cf9b3e
Aleezitu <alee_meier@outlook.com>
2023-06-07 19:42:39 -0300

    Adicionar arquivos de projeto.

---
+++ ACPEFINAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ACPEFINAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
